945abebbee89a7e621d0689f3e582be5c0f374fc
is-nrdra <isanoorendra@gmail.com>
2022-07-02 19:25:13 +0000

    X00TD: Add RemovePackages to device makefile

--- device.mk
+++ device.mk
@@ -20,6 +20,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     init.device.rc
 
+# RemovePackages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Thermal configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
