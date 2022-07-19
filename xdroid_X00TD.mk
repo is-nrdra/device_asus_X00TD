af9e74a62202da8015e1138aa5a456b2f92e2b4d
is-nrdra <isanoorendra@gmail.com>
2022-07-19 08:47:40 +0000

    X00TD: Update Build Fingerprint with Raven - July 2022
    Signed-off-by: is-nrdra <isanoorendra@gmail.com>

--- xdroid_X00TD.mk
+++ xdroid_X00TD.mk
@@ -29,7 +29,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220605.009.B1 8650216 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003.A1 8672226 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ3A.220705.003.A1/8672226:user/release-keys"
